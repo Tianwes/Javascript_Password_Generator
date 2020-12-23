cf209e6b81a8ad0634342050bfdd35145fa9a76e
Tianwes <willnotsuffice@gmail.com>
2020-12-23 16:11:26 -0500

    Affirmative and negative prompt/confirms work down the list, need to store input and randomize array, need to restart q's if all answered negative.

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Should I make lists a JS object?
-// var charOptions = {}
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var specialChar = "!@#$%&*?-+=";
-// ^need to store passwordLength
-// (i=0; i>7 && i<129; i++)
-// need it to be 8-128 char
-// Math.random().toString(128).slice(8)
+
+
 function generatePassword() {
-  var passwordLength = prompt("Please pick a password length between 8-128 characters.");
-  // length needs to be 8 or greater, and 128 or less
-  while(passwordLength < 8 || passwordLength > 128){
-  // if(passwordLength < 8 || passwordLength > 128){
-    alert("Please pick a password length between 8 and 128 characters.");
+
+  var passwordLength = prompt("Please pick a password length between 8-128 characters.")
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid");
     var passwordLength = prompt("Please pick a password length between 8-128 characters.");
-  // }else if(passwordLength > 7 && passwordLength < 129){
-  //    alert("Thank you."); 
-  //   }
-   }
-   while(passwordLength > 7 && passwordLength < 129){
-    alert("Thank you."); 
-    break
-   }
-   var lowerCaseConfirm = confirm("Do you want your password to contain lowercase characters?")
-   if(lowerCaseConfirm === true){
-     alert("Got it, your password will contain lowers case characters!");
-   }else{
-     alert("No lowercase characters will be used.");
-   }
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      break;
+    }
   }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Thanks!");
+    var lowerCaseConfirm = confirm("Would you like your password to contain lowercase letters?");
+  }
+  if (lowerCaseConfirm === true) {
+    alert("Your password will contain lowercase letters!");
+    var upperCaseConfirm = confirm("Would you like your password to contain uppercase letters?");
+  } else if (lowerCaseConfirm === false) {
+    alert("Your password will NOT contain lowercase letters!");
+    var upperCaseConfirm = confirm("Would you like your password to contain uppercase letters?");
+  }
+  if (upperCaseConfirm === true) {
+    alert("Your password will contain uppercase letters!");
+    var numConfirm = confirm("Would you like your password to contain numbers?");
+  } else if (upperCaseConfirm === false) {
+    alert("Your password will NOT contain uppercase letters!");
+    var numConfirm = confirm("Would you like your password to contain numbers?");
+  }
+  if (numConfirm === true) {
+    alert("Your password will contain numbers!");
+    var specialCharConfirm = confirm("Would you like your password to contain special characters?");
+  } else if (numConfirm === false) {
+    alert("Your password will NOT contain numbers!");
+    var specialCharConfirm = confirm("Would you like your password to contain special characters?");
+  }
+  if (specialCharConfirm === true) {
+    alert("Your password will contain special characters!");
+  } else if (specialCharConfirm === false) {
+    alert("Your password will NOT contain special characters!");
+  }
+  
   
 
 
-
-
-
-
-
-
-
-
-
+}
 
 
 
